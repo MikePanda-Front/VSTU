b2dfd0f48572d3fe3809c154520b7cecb2715db4
MikePanda-Front <Dima1997.s@yandex.ru>
2020-10-22 19:14:00 +0300

    Configs -add linter config -add prettier config

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+<p>test</p>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
 </script>
 
 <style>
---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style></style>
